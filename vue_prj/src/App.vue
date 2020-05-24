ad0b74d2f26ce7de765aee550281ac93851428e6
ghanthony <niluoh@qq.com>
2020-05-25 07:21:20 +0800

    feat: 添加vue实现的options页面

---
+++ vue_prj/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Options/>
+  </div>
+</template>
+
+<script>
+import Options from './components/Options'
+
+export default {
+  name: 'App',
+  components: {
+    Options
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
