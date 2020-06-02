6684425ae94cc93631039ec18af4a9f2995a5615
hou.lin <hou.lin@opg.cn>
2020-06-02 18:05:46 +0800

    feat: 提交62进度

--- vue_prj/src/components/about.vue
+++ vue_prj/src/components/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card style="max-width: 840px">
     <li class="about_line"> {{author}} <a href="https://www.weibo.com/2627808625/profile?topnav=1&wvr=6" target="_blank">@青野侯</a></li>
     <li class="about_line">
       <a href="https://github.com/ghanthony/rightKeyCustomSearch/issues" target="_blank"> {{advice}} </a>
--- vue_prj/src/components/search.vue
+++ vue_prj/src/components/search.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-      <el-card>
+      <el-card style="max-width: 840px">
         <el-row>
           <el-col class="col" :span="24">{{searchExampleLabel}}</el-col>
         </el-row>
         <el-row :gutter="20" style="max-width: 800px">
-          <el-col class="col" :span="3">
+          <el-col class="col" :span="4">
             <el-input
               readonly="true"
               :value="searchExampleNameLabel"
               :disabled="true">
             </el-input>
           </el-col>
-          <el-col class="col" :span="12">
+          <el-col class="col" :span="13">
             <el-input
               readonly="true"
               :value="searchExampleUrlLabel"
@@ -20,24 +20,49 @@
             </el-input>
           </el-col>
         </el-row>
-        <el-row  style="max-width: 800px">
-        <el-table :data="tableData" header-align="center">
-          <el-table-column
-            class=""
-            prop="name"
-            :label="searchNameLabel"
-            width='100px'>
-          </el-table-column>
-          <el-table-column
-            prop="url"
-            :label="searchUrlLabel"
-            width='350px'>
-          </el-table-column>
-           <el-table-column
-            prop="url"
-            width='350px'>
-          </el-table-column>
-        </el-table>
+        <el-row  style="max-width: 800px;">
+          <el-table :data="tableData" header-align="center">
+            <el-table-column
+              prop="name"
+              :label="searchNameLabel"
+              width='133px'>
+            </el-table-column>
+            <el-table-column
+              prop="url"
+              :label="searchUrlLabel"
+              width='483px'>
+            </el-table-column>
+            <el-table-column
+              prop="url"
+              fixed="right"
+              :label="searchOpLabel"
+              width='183px'>
+              <template slot-scope="scope">
+                <el-button
+                  size="small"
+                  type="primary"
+                  icon="el-icon-edit"
+                  @click="handleEdit(scope.$index, scope.row)"></el-button>
+                <el-button
+                  size="small"
+                  type="danger"
+                  icon="el-icon-delete"
+                  @click="handleDelete(scope.$index, scope.row)"></el-button>
+                <el-button
+                  size="small"
+                  type="info"
+                  icon="el-icon-sort"></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-row>
+        <el-row>
+          <el-col class="col" :span="24">
+            <el-button
+              icon="el-icon-plus"
+              style="width: 100%; border-radius: 0px; margin-top : -1px; border-style: dotted"
+              @click="handleAdd()"></el-button>
+          </el-col>
         </el-row>
       </el-card>
   </div>
@@ -52,15 +77,18 @@ export default {
       searchExampleUrlLabel: 'http://s.weibo.com/user/%s&Refer=SUer_box',
       searchNameLabel: '名字',
       searchUrlLabel: '链接',
-      tableData: [{
-        name: '百度',
-        url: 'www.baidu.com'
-      }, {
-        name: '百度',
-        url: 'www.baidu.com'
-      }
-
+      searchOpLabel: '操作',
+      tableData: [
       ]
+    }
+  },
+  methods: {
+    handleEdit () {
+
+    },
+    handleDelete () {
+    },
+    handleAdd () {
     }
   }
 }
