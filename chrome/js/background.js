4d1af44e1db9b2e54dbca717c9181c084b0c456f
hou.lin <hou.lin@opg.cn>
2020-06-04 17:57:26 +0800

    fix: 删除非必要文件

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,5 +1,9 @@
 ﻿function onClickHandler(info, tab) {
-  if (info.menuItemId == "googleSearch") {
+  if (info.menuItemId == "hightligh" ) {
+    chrome.tabs.sendMessage(tab.id, {func:"highlight",arg:info.selectionText});
+  } else if (info.menuItemId == "unHightligh") {
+    chrome.tabs.sendMessage(tab.id, {func:"unHighlight",arg:info.selectionText});
+  } else if (info.menuItemId == "googleSearch") {
     chrome.tabs.create({active:true,index:tab.index+1,url:"http://www.google.com#q="+info.selectionText});
   } else if (info.menuItemId == "weiboSearch") {
     chrome.tabs.create({active:true,index:tab.index+1,url:"http://s.weibo.com/user/"+ info.selectionText +"&Refer=SUer_box"});
@@ -14,16 +18,12 @@
   }
 };
 
+var hightligh = chrome.contextMenus.create({"title": "高亮选中文本", "contexts":["selection"],"id":"hightligh"});
 var googleSearch = chrome.contextMenus.create({"title": "谷歌","contexts":["selection"],"id":"googleSearch"});
 var googleSearch = chrome.contextMenus.create({"title": "虾米音乐","contexts":["selection"],"id":"xiamiSearch"});
 var googleSearch = chrome.contextMenus.create({"title": "豆瓣电影","contexts":["selection"],"id":"doubanMovieSearch"});
 var googleSearch = chrome.contextMenus.create({"title": "淘宝找货","contexts":["selection"],"id":"taobaoSearch"});
 var googleSearch = chrome.contextMenus.create({"title": "微博找人","contexts":["selection"],"id":"weiboSearch"});
-
+var unhightligh = chrome.contextMenus.create({"title": "清除高亮", "contexts":["all"],"id":"unHightligh"});
 chrome.contextMenus.onClicked.addListener(onClickHandler);
-
-chrome.browserAction.onClicked.addListener(
-  function(tab) {
-    chrome.tabs.create({url:'options.html'});
-});
 
